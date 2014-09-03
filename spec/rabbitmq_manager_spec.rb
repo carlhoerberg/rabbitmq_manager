b2e25866176f7f4d008a8667aa09ae4b11aaeb7c
Adam Straughan <adam.straughan@comparethemarket.com>
2014-09-03 15:22:41 +0100

    upgraded to rspec 3.0.0

--- spec/rabbitmq_manager_spec.rb
+++ spec/rabbitmq_manager_spec.rb
@@ -1,103 +1,131 @@
 require './spec/spec_helper'
 require './lib/rabbitmq_manager'
 
-describe RabbitMQManager do
-  let(:manager) { 
-    RabbitMQManager.new 'http://guest:guest@localhost:15672'
-  }
+def create_manager
+  RabbitMQManager.new 'http://guest:guest@localhost:15672'
+end
 
-  context '#overview' do
-    subject { manager.overview }
-    it { should be_instance_of Hash }
+RSpec.describe RabbitMQManager do
+  before(:all) do
+    @manager = create_manager
   end
 
-  context '#connections' do
-    subject { manager.connections }
-    it { should have_at_least(1).things }
+  it "the overview method provides a Hash" do
+    expect(@manager.overview()).to be_a Hash
   end
 
-  context '#channels' do
-    subject { manager.channels }
-    it { should have_at_least(1).things }
+  it "there will be at least one connection" do
+    items = @manager.connections()
+    expect(items.length).to be >= 1
   end
 
-  context '#nodes' do 
-    subject { manager.nodes }
-    it { should have(1).things }
+  it "there will be at least one channel" do
+    items = @manager.channels()
+    expect(items.length).to be >= 1
   end
 
-  context '#queues' do
-    subject { manager.queues }
-    it { should be_instance_of Array }
+  describe "node discovery" do
+    it "there will be at least one node" do
+      items = @manager.nodes()
+      expect(items.length).to be >= 1
+    end
+    it "" do
+        hostname = `hostname`.chop
+        item = @manager.node("rabbit@#{hostname}")
+        expect(item).to be_a Hash
+    end
   end
 
-  context '#queues(vhost)' do
-    subject { manager.queues('/') }
-    it { should be_instance_of Array }
+  it "all queues can be listed as an Array" do
+    items = @manager.queues()
+    expect(items).to be_an Array
   end
 
-  context '#queue' do
-    let(:queue) { 'q1' }
-    before { manager.queue_create('/', queue) }
-    subject { manager.queue('/', queue) }
-    it { should be_instance_of Hash }
-    it { subject['name'].should == queue }
-    after { manager.queue_delete('/', queue) }
+  it "queues can be listed per vhost as an Array" do
+    items = @manager.queues('/')
+    expect(items).to be_an Array
   end
 
-  context '#node' do
-    let(:hostname) { `hostname`.chop }
-    subject { manager.node("rabbit@#{hostname}") }
-    it { should be_instance_of Hash }
-  end
+  describe "queue manipulation" do
+    queue = 'rspec-q1-' + rand(1000).to_s
+    vhost = '/'
 
-  context 'when administering users and vhosts' do
-    let(:user)  { 'user1' }
-    let(:passwd)  { 'rand123' }
-    let(:vhost) { 'vh1' }
-
-    before do
-      manager.user_create user, passwd
-      manager.vhost_create vhost
+    after(:all) do
+      @manager.queue_delete(vhost, queue)
     end
 
-    after do
-      manager.user_delete user
-      manager.vhost_delete vhost
+    it "queues can be created and removed" do
+      @manager.queue_create(vhost, queue)
+      items = @manager.queue(vhost, queue)
+      expect(items).to be_a Hash
+      expect(items['name']).to eq(queue)
     end
-    
-    it 'can list vhosts' do
-      manager.vhosts.should have_at_least(2).things
+  end
+
+  describe "when administering users and vhosts" do
+    user = 'rspec-user-' + rand(1000).to_s
+    passwd = rand(1000).to_s
+    vhost = 'rspec-vh'
+
+    before(:all) do
+      @manager.user_create user, passwd
+      @manager.vhost_create vhost
+    end
+
+    after(:all) do
+      @manager.user_delete user
+      @manager.vhost_delete vhost
+    end
+
+    it "list vhosts" do
+      expect(@manager.vhosts.length).to be >= 2
     end
 
     it 'can view one vhost' do 
-      manager.vhost(vhost)['name'].should == vhost
+      expect(@manager.vhost(vhost)['name']).to eq(vhost)
     end
 
     it 'can list users' do 
-      manager.users.should have_at_least(2).things
+      expect(@manager.users.length).to be >= 2
     end
 
     it 'can view one user' do 
-      manager.user(user)['name'].should == user
+      expect(@manager.user(user)['name']).to eq(user)
     end
 
     it 'cannot view an non existing user' do
-      lambda {
-        manager.user('foo')
-      }.should raise_error Faraday::Error::ResourceNotFound
+      expect {
+        @manager.user('foo')
+      }.to raise_error Faraday::Error::ResourceNotFound
     end
 
-    context 'permissions' do
-      before  { manager.user_set_permissions(user, vhost, '.*', '.*', '.*') }
-      subject { manager.user_permissions(user) }
-      it { subject.should be_instance_of Array }
-      context 'for first element' do
-        subject { manager.user_permissions(user).first }
-        it('has name') { subject['user'] == user }
-        it('has read permissions') { subject['read'] == '.*' }
-        it('has write permissions') { subject['write'] == '.*' }
+    describe 'when administering users and vhosts' do
+      users = []
+      first = nil
+
+      before(:all) do
+        @manager.user_set_permissions(user, vhost, '.*', '.*', '.*')
+        users = @manager.user_permissions(user)
+        first = users.first
       end
+      
+      it 'user permissions is an Array' do
+        expect(users).to be_an Array
+      end
+
+      it 'can read first users name' do
+        expect(first['user']).to eq(user)
+      end
+
+      it 'first users has read permissions' do
+        expect(first['read']).to eq('.*')
+      end
+
+      it 'first users has write permissions' do
+        expect(first['write']).to eq('.*')
+      end
+
     end
+
   end
 end
